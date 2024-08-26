35d0a44102d561610b2ce26ed06df3a1df458651
cklog <csknowledgelog@gmail.com>
2024-08-26 13:13:12 +0900

    Update 151137.sql

--- programmers/151137.sql
+++ programmers/151137.sql
@@ -1,6 +1,3 @@
--- https://school.programmers.co.kr/learn/courses/30/lessons/151137
--- 자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기
-
 SELECT CAR_TYPE, COUNT(CAR_TYPE) AS CARS
 FROM CAR_RENTAL_COMPANY_CAR
 WHERE OPTIONS LIKE '%시트%'
