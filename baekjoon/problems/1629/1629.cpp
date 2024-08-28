73d14de1382e6fe701b9a07139a315f01d6ed344
cklog <csknowledgelog@gmail.com>
2024-08-28 20:00:36 +0900

    Create 1629.cpp

---
+++ baekjoon/problems/1629/1629.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long a,b,c;
+
+long long calc(long long a, long long b){
+    if(b==1) return a%c;
+    long long ret=calc(a,b/2);
+    ret=(ret*ret)%c;
+    if(b%2) ret=(ret*a)%c;
+    return ret;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin>>a>>b>>c;
+    cout<<calc(a,b);
+}
